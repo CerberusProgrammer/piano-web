3caa9d78f35c67c1a2c44ae279500a6f2732e743
Omar Flores Salazar <cerberusprogrammer@gmail.com>
2020-07-22 14:49:12 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+function playC() {
+    var audio = document.getElementById("c");
+    audio.play();
+}
+
+function playCs() {
+    var audio = document.getElementById("cs");
+    audio.play();
+}
+
+function playD() {
+    var audio = document.getElementById("d");
+    audio.play();
+}
+
+function playDs() {
+    var audio = document.getElementById("ds");
+    audio.play();
+}
+
+function playE() {
+    var audio = document.getElementById("e");
+    audio.play();
+}
+
+function playF() {
+    var audio = document.getElementById("f");
+    audio.play();
+}
+
+function playFs() {
+    var audio = document.getElementById("fs");
+    audio.play();
+}
+
+function playG() {
+    var audio = document.getElementById("g");
+    audio.play();
+}
+
+function playGs() {
+    var audio = document.getElementById("gs");
+    audio.play();
+}
+
+function playA() {
+    var audio = document.getElementById("a");
+    audio.play();
+}
+
+function playAs() {
+    var audio = document.getElementById("as");
+    audio.play();
+}
+
+function playB() {
+    var audio = document.getElementById("b");
+    audio.play();
+}
